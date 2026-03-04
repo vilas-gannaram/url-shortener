310552db4a5280fe77cab7d647f26bbaca29b027
vilas-gannaram <vilasgannaram@gmail.com>
2026-03-04 22:06:30 +0530

    feat: Configure SQLite for WAL mode to enhance concurrency, update database model and gitignore.

--- internal/storage/db.go
+++ internal/storage/db.go
@@ -15,8 +15,8 @@ type URLStats struct {
 	ID              uint       `gorm:"column:id;primaryKey"`
 	URLMappingID    uint       `gorm:"column:url_mapping_id;not null"`
 	URLMapping      URLMapping `gorm:"column:url_mapping_id;foreignKey:URLMappingID"`
-	RedirectedCount int        `gorm:"column:redirected_count;not null"`
-	LastUpdated     int64      `gorm:"column:last_updated;not null"`
+	RedirectedCount int        `gorm:"column:redirected_count;not null;default:0;"`
+	LastUpdated     int64      `gorm:"column:last_updated;autoUpdateTime:milli"`
 }
 
 func InitDB() *gorm.DB {
@@ -25,6 +25,21 @@ func InitDB() *gorm.DB {
 	if err != nil {
 		panic("failed to connect database: " + err.Error())
 	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		panic("failed to get sql db: " + err.Error())
+
+	}
+
+	// Set max open connections to 1
+	sqlDB.SetMaxOpenConns(1)
+
+	// Enable WAL mode for better concurrency
+	// Other users can read, while one can write
+	db.Exec("PRAGMA journal_mode=WAL;")
+	db.Exec("PRAGMA synchronous=NORMAL;")
+	db.Exec("PRAGMA busy_timeout=5000;")
 
 	db.AutoMigrate(&URLMapping{}, &URLStats{})
 	return db
